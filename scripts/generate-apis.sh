d1921e2315ab4bc517c15483a7cbe64ec11fb686
karurochari <nope>
2022-10-01 04:58:39 +0000

    Prepared openapi@

--- scripts/generate-apis.sh
+++ scripts/generate-apis.sh
@@ -1,7 +1,10 @@
 #/bin/bash
 
 rm -rf .src.old
-mkdir src.old
+mkdir .src.old
 cp -r src/api.auto .src.old/api.auto
+rm -rf src/api.auto/*
 
 #Perform destructive changes on the original code base
+
+openapi-generator-cli generate -i api/service.openapi.yaml -g typescript -o src/api.auto
