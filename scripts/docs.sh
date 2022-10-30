5abaa19515480b2a1140df89f09a11d100f7d6bf
karurochari <nope>
2022-10-30 19:40:14 +0000

    Improved the structure of the documentation generated by godot

--- scripts/docs.sh
+++ scripts/docs.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 rm -r ./docs/content/schemas/* 2>/dev/null
+rm -r ./docs/static/schemas/* 2>/dev/null
+rm -r ./docs/dist/docs/* 2>/dev/null
 
 
-jsonschema2md -d ./src/auto-schemas/ -o ./docs/content/schemas -e 'json'
+jsonschema2md -d ./src/auto-schemas/ -o ./docs/content/schemas -x ./docs/static/schemas
+touch ./docs/content/schemas/_index.md
+
 cd ./docs && hugo --destination ../dist/docs
